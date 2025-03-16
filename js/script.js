ddb031542fa2fbcc2de49211c2fef8379f481f4f
Loug <lucianoguasque@gmail.com>
2025-03-16 07:31:20 -0300

    small update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-let programmer = {
+/* const programmer = {
   name: "Loug",
   preferredLanguage: "JavaScript",
   writeCode: function () {
     return `${this.name} codes using ${this.preferredLanguage}`;
   },
-};
+}; 
+console.log(programmer.writeCode());*/
 
-console.log(programmer.writeCode());
-
-let groceryListItem = {
+/* let groceryListItem = {
   fruit: "apple",
   quantity: 4,
   display: function () {
     console.log(`There are ${this.quantity} ${this.fruit}s`);
   },
 };
-groceryListItem.display();
+groceryListItem.display(); */
+
+class Programmer {
+  constructor(name, language) {
+    this.name = name;
+    this.language = language;
+  }
+
+  writeCode() {
+    console.log(`${this.name} is a ${this.language} programmer.`);
+  }
+}
+
+const coder = new Programmer("Loug", "javaScript");
+
+coder.writeCode();
