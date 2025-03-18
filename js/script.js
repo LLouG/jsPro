7b0d1626c007a9d2aeb1f08faafab860d967674d
Loug <lucianoguasque@gmail.com>
2025-03-18 05:43:39 -0300

    Added abstraction

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const performCalc = calcPrice;
 console.log(performCalc(newItem, 0.25));
 console.log(calcPrice(newItem, 0.2)); */
 
-const colors = ["Red", "Green", "Blue", "Rebeccapurple", "Firebrick"];
+/* const colors = ["Red", "Green", "Blue", "Rebeccapurple", "Firebrick"];
 
 for (const i in colors) {
   console.log(`${i} - ${colors[i]}`);
@@ -27,4 +27,67 @@ for (const key in dog) console.log(dog[key]);
 
 for (const keys of Object.keys(dog)) {
   console.log(keys);
+} */
+
+/* // With Abstraction
+function SayHello(name, greeting = "Hello") {
+  this.name = name;
+  this.greeting = greeting;
+
+  // Public method
+  this.returnHello = () => {
+    return `${this.greeting}, ${this.name}`;
+  };
+
+  // Private method
+  const drinksCoffee = function () {
+    console.log(`${this.name} drinks coffee.`);
+  }.bind(this);
+
+  // Public method
+  this.startDay = () => {
+    drinksCoffee();
+  };
 }
+
+const loug = new SayHello("Loug");
+console.log(loug.returnHello());
+
+const haejoon = new SayHello("Haejoon");
+console.log(haejoon.returnHello());
+haejoon.startDay(); */
+
+function GroceryItem(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
+
+  this.display = () => {
+    console.log(`${this.name} x ${this.quantity}`);
+  };
+}
+
+function GroceryList() {
+  const items = []; // Private array to store items
+
+  const calcItemQuantity = () => {
+    return items.reduce((total, item) => total + item.quantity, 0);
+  };
+
+  this.addItem = (name, quantity) => {
+    const item = new GroceryItem(name, quantity);
+    items.push(item);
+  };
+
+  this.displayItems = () => {
+    items.forEach((item) => item.display());
+  };
+
+  this.getTotalQuantity = () => {
+    return calcItemQuantity();
+  };
+}
+
+const myList = new GroceryList();
+myList.addItem("Bananas", 5);
+myList.addItem("Apples", 3);
+myList.displayItems();
